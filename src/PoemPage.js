ef0b4d20af24ceec31c7211371a0c5ce7f72d95f
Sterling Watts <SterlingDWatts@gmail.com>
2019-11-18 14:45:15 -0800

    Added Links

--- src/PoemPage.js
+++ src/PoemPage.js
@@ -2,17 +2,17 @@ import React from 'react'
 import POEMS from './poems'
 
 export default function PoemPage(props) {
-  const poem = POEMS.find(p =>
-    p.id === props.match.params.poemId
-  )
-  return (
-    <article className='Poem'>
-      <h2>{poem.title}</h2>
-      {poem.content.map((p, i) =>
-        (p === '')
-          ? <br key={i} />
-          : <p key={i}>{p}</p>
-      )}
-    </article>
-  )
+    const poem = POEMS.find(p =>
+        p.id === props.match.params.poemId
+    )
+    return (
+        <article className='Poem'>
+            <h2>{poem.title}</h2>
+            {poem.content.map((p, i) =>
+                (p === '')
+                    ? <br key={i} />
+                    : <p key={i}>{p}</p>
+            )}
+        </article>
+    )
 }
